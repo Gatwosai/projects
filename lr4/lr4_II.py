5cafd6e5402e2711f468f1d507ff70a99a8967b2
Aleksandr Belyaev <leopard7462@gmail.com>
2022-02-27 22:27:52 +0700

    Implemented k-means method for image

--- lr4/lr4_II.py
+++ lr4/lr4_II.py
@@ -1,17 +1,33 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.cbook as cbook
 
-NUMBER_OF_PIXEL = 100
+PATH_OF_FILE = '/home/gasai/Документы/учеба/ИИ/projects/lr4/naruto.jpg'
 K = 10
 
 def main():
-    pixels = np.random.randint(0, 255 + 1, size=(NUMBER_OF_PIXEL, 3))
-    k_means_for_pixels(pixels)
+    global K
+    while True:
+        input_k = input('Ввод количества цветов: ')
+        if input_k.isdigit() == True:
+            if int(input_k) > 0:
+                break
+        print('Неверный ввод, ожидалось число большее 0')
+    K = int(input_k)
+    show_images()
     
-def rand_pixel():
-    pass
+def show_images():
+    """Shows uploaded and converted image."""
+    with cbook.get_sample_data(PATH_OF_FILE) as image_file:
+        image = plt.imread(image_file)
+    plt.figure(1)
+    plt.title('Начальное изображение')
+    plt.imshow(image)
+    plt.figure(2)
+    plt.title('Преобразованное изображение')
+    plt.imshow(k_means_for_image(image))
+    plt.show()
 
 def pixel_bright(pix1, pix2):
     """Evaluates the proximity of pixel brightness.
@@ -21,14 +37,14 @@ def pixel_bright(pix1, pix2):
     red, green and blue colors.
 
     """
-    R1, G1, B1 = pix1[0], pix1[1], pix1[2]
-    R2, G2, B2 = pix2[0], pix2[1], pix2[2]
+    R1, G1, B1 = int(pix1[0]), int(pix1[1]), int(pix1[2])
+    R2, G2, B2 = int(pix2[0]), int(pix2[1]), int(pix2[2])
     return 30 * (R1-R2)**2 + 59 * (G1-G2)**2 + 11 * (B1-B2)**2
 
 def estimate_center(cluster):
     """Find and return mean RGB value in cluster."""
     R, G, B = 0, 0, 0
-    len_cluster = len(cluster)
+    len_cluster = len(cluster) if len(cluster) > 0 else 1
     for pixel in cluster:
         R += pixel[0]
         G += pixel[1]
@@ -37,50 +53,26 @@ def estimate_center(cluster):
     G = round(G / len_cluster)
     B = round(B / len_cluster)
     return [R, G, B]
-
-def visualisation_2d(clusters):
-    """Show 2D scatter of clusters."""
-    k = len(clusters)
-    plt.grid() 
-    plt.xlabel("x")    
-    plt.ylabel("y")
-    for i in range(k):
-        x_coordinates = []
-        y_coordinates = []
-        for q in range(len(clusters[i])):
-            x_coordinates.append(clusters[i][q][0])
-            y_coordinates.append(clusters[i][q][1])
-        plt.scatter(x_coordinates, y_coordinates)
-    plt.show()
     
-def visualisation_3d(clusters):
-    """Show 3D scatter of clusters."""
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    for i in range(len(clusters)):
-        for j in range(len(clusters[i])):
-            R = clusters[i][j][0]
-            G = clusters[i][j][1]
-            B = clusters[i][j][2]
-            ax.scatter(R, G, B, color=(R / 255, G / 255, B / 255))
-    ax.set_xlabel('R')
-    ax.set_ylabel('G')
-    ax.set_zlabel('B')
-    plt.show()
-    
-def k_means_for_pixels(pixels):
+def k_means_for_image(image):
     """Method k-means for pixels."""
     # 1 point
-    samples = random.sample(range(0, NUMBER_OF_PIXEL), K)
-    centers = [pixels[s] for s in samples]
+    samples_x = random.sample(range(0, image.shape[0]), K)
+    samples_y = random.sample(range(0, image.shape[0]), K)
+    centers = [image[x][y] for x, y in zip(samples_x, samples_y)]
     clusters = [[]] * K
-    while True:
+    new_image = image[:]
+    i = 0
+    while i < 20:
+        i += 1
         clusters0 = clusters[:]
         clusters = [[] for _ in range(K)]
         # 2 point
-        for pixel in pixels:
-            proximity = [pixel_bright(pixel, c) for c in centers]
-            clusters[np.argmin(proximity)].append(pixel)
+        for i in range(len(image)):
+            for j in range(len(image[i])):
+                proximity = [pixel_bright(image[i][j], c) for c in centers]
+                clusters[np.argmin(proximity)].append(image[i][j])
+                new_image[i][j] = centers[np.argmin(proximity)]
         # 3 point
         for i in range(K):
             centers[i] = estimate_center(clusters[i])
@@ -94,9 +86,10 @@ def k_means_for_pixels(pixels):
                 for k in range(len(clusters[i][j])):
                     if clusters[i][j][k] != clusters0[i][j][k]:
                         check = False
+                        break
         if check:
             break
-        visualisation_3d(clusters)
+    return new_image
     
 if __name__ == '__main__':
     main()
