b8aafdc2b85a888642384611d8bb07f344ef7197
Aleksandr Belyaev <leopard7462@gmail.com>
2022-03-31 10:46:51 +0700

    begin

---
+++ OM7/main.py
@@ -0,0 +1,18 @@
+import random
+
+
+def main():
+    r = random.random()
+    print(r)
+
+
+def density_distr():
+    pass
+
+
+def gen_random():
+    pass
+
+
+if __name__ == "__main__":
+    main()
